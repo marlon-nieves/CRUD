ecc53f3ff5fa35842a8881b9b68abfeb5b6a095a
marlon-nieves <marlon.3801711612@ucaldas.edu.co>
2022-04-26 20:26:02 -0500

    changes form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {MatDialog, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { DialogComponent } from './dialog/dialog.component';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'CRUD';
+  title = 'CRUD ANGULAR';
+
+  constructor(private dialog : MatDialog){
+
+  }
+  openDialog(){
+    this.dialog.open(DialogComponent, {
+    width:'30%'
+    });
+  }
 }
